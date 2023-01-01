37046d7a3f4ff7b14d11049d2301e490c1c3814a
titi-lima <titisau@gmail.com>
2023-01-01 04:55:24 -0300

    finished (?)

--- AP1/kernel.asm
+++ AP1/kernel.asm
@@ -4,7 +4,8 @@ ORG 0x7C00
 BITS 16
     jmp start
 
-msg: db "hello world", 0x0D, 0x0A, 0
+msg: db "Essa mensagem tem, ao todo, 13 vogais", 0x0D, 0x0A, 0
+display_string: db "Numero de vogais: ", 0
 
 start:
 
@@ -12,48 +13,81 @@ start:
     mov ds, ax
     mov es, ax
     mov ss, ax
-
-    mov si, msg
+    mov si, display_string
     call print_string
-
-    xor ax, ax
-    mov ax, 101
+    mov si, msg
+    call count_vowels
+    mov ax, cx
     call print_number
+
+count_vowels:
+    mov cx, 0
+    .loop:
+        lodsb
+        or al, al ; checa se acabou a string
+        jz .done
+        cmp al, 'a'
+        je .vowel
+        cmp al, 'e'
+        je .vowel
+        cmp al, 'i'
+        je .vowel
+        cmp al, 'o'
+        je .vowel
+        cmp al, 'u'
+        je .vowel
+        cmp al, 'A'
+        je .vowel
+        cmp al, 'E'
+        je .vowel
+        cmp al, 'I'
+        je .vowel
+        cmp al, 'O'
+        je .vowel
+        cmp al, 'U'
+        je .vowel
+        jmp .loop
+    .vowel:
+        inc cx ; incrementa o contador a cada vogal encontrada
+        jmp .loop
+    .done:
+        mov ax, cx
+        ret
 
 end:
     jmp $ ; halt
 
 
 print_string:
-.loop:
-    lodsb
-    or al, al
-    jz .done
-    mov ah, 0x0E
-    int 0x10
-    jmp .loop
-.done:
-    ret
+    .loop:
+        lodsb
+        or al, al
+        jz .done
+        mov ah, 0x0E
+        int 0x10
+        jmp .loop
+    .done:
+        ret
 
 print_number:
     mov bx, 10
     mov cx, 0
-.loop1:
-    mov dx, 0
-    div bx
-    ; resposta vai ta no ax, resto no dx
-    add dx, 48
-    push dx
-    inc cx
-    cmp ax, 0
-    jne .loop1
-.loop2:
-    pop ax
-    mov ah, 0x0E
-    int 0x10
-    loop .loop2
-.done:
-    ret
+    .loop1:
+        mov dx, 0
+        div bx
+        ; resposta vai ta no ax, resto no dx
+        add dx, 48
+        push dx
+        inc cx
+        cmp ax, 0
+        jne .loop1
+    .loop2:
+        pop ax
+        mov ah, 0x0E
+        int 0x10
+        loop .loop2
+    .done:
+        ret
 
 ; assinatura de boot
 
